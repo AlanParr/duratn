11b1b0c4c42eb6f069c7dfdd9f6733dc5100dac3
Alan Parr <alan@stewie>
2013-09-03 21:17:27 +0100

    Initial commit

---
+++ duratn.js
@@ -0,0 +1,85 @@
+﻿(function($) {
+
+$.fn.jqDuration = function () {
+    //Add 3 new controls for days,hrs,mins.
+    var container = this.parent();
+
+    var originatingElement = this;
+    var thisInstanceElementGuid = generateGuid();
+    var contId = 'jqDuration_' + thisInstanceElementGuid;
+    var daysId = 'jqDuration_days_' + thisInstanceElementGuid;
+    var hrsId = 'jqDuration_hrs_' + thisInstanceElementGuid;
+    var minsId = 'jqDuration_mins_' + thisInstanceElementGuid;
+
+    var jqDurationContainer = $('<div>').attr({
+        id: contId,
+        name: contId
+    }).addClass("duration-container");
+
+    $('<input>').attr({
+        type: 'number',
+        id: daysId,
+        name: daysId,
+        placeholder: "days"
+    }).appendTo(jqDurationContainer);
+
+    $('<input>').attr({
+        type: 'number',
+        id: hrsId,
+        name: hrsId,
+        placeholder: "hrs"
+    }).appendTo(jqDurationContainer);
+
+    var mins = {
+        '0': '0',
+        '15': '15',
+        '30': '30',
+        '45': '45'
+    };
+    
+    var minsInput = $('<select>').attr({
+        type: 'number',
+        id: minsId,
+        name: minsId,
+        placeholder: "mins"
+    });
+    for (var val in mins) {
+        $('<option />', { value: val, text: mins[val] + ' mins' }).appendTo(minsInput);
+    }
+    minsInput.appendTo(jqDurationContainer);
+
+    jqDurationContainer.appendTo(container);
+
+    $('#' + daysId).bind("click", recalculate);
+    $('#' + hrsId).bind("click", recalculate);
+    $('#' + minsId).bind("click", recalculate);
+
+    //Hide this
+    this.hide();
+
+    return this;
+
+    function generateGuid() {
+        var result = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+        return result;
+    }
+    
+    function recalculate(event) {
+        var vals = new Array();
+        
+        if ($('#' + daysId).val() != "") { vals.push(parseInt($('#' + daysId).val() * 480));}
+        if ($('#' + hrsId).val() != "") { vals.push(parseInt($('#' + hrsId).val() * 60)); }
+        if ($('#' + minsId).val() != "") { vals.push(parseInt($('#' + minsId).val())); }
+        
+        var result = vals.reduce(function (previousValue, currentValue) {
+            return currentValue + previousValue;
+        });
+
+        $(originatingElement).val(result);
+    }
+};
+
+}(jQuery));
---
+++ readme.md
@@ -0,0 +1,3 @@
+Duratn
+
+Duratn is a simple jQuery plugin that presents the user with a simple days, hours and minutes method for selecting a duration which is then aggregated into minutes for the application.
